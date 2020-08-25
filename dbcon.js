f4ad9c2e6cce9151f111bcd68a33eaf702c918b9
Kevin Joy <kevinjoy@kevins-mbp.home>
2020-08-24 23:37:19 -0500

    clear db credentials

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs290_joyke',
-  password        : '7901',
-  database        : 'cs290_joyke'
+  host            : 'us-cdbr-east-02.cleardb.com',
+  user            : 'bb7c32d165ca39',
+  password        : '302f9dce',
+  // database        : 'cs290_joyke'
 });
 
 module.exports.pool = pool;
